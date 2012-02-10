31a1fa069025dceefd47296d05ef211b285b34db
Michael Blackmon <differentiablef@gmail.com>
2012-02-10 06:35:57 -0500

    Implmented add_article and added template for edit_article

--- modules/articles/views.py
+++ modules/articles/views.py
@@ -9,12 +9,17 @@ from models import Article, Article_Comment
 
 from flask import render_template
 
-# Local imports
+# ##############################################################################
+# Name: default
+# Synop: main entry point for module
 @articles_mod.route("/view")
 def default():
     arts = Article.query.all()
     return render_template("view_articles.html", articles = arts)
 
+# ##############################################################################
+# Name: view_article
+# Synop: Display a particular article
 @articles_mod.route("/view/<int:articleid>")
 def view_article(articleid):
     art = Article.query.get(articleid)
@@ -24,25 +29,51 @@ def view_article(articleid):
     
     return render_template("view_article.html", article = art)
 
+# ##############################################################################
+# Name: update_articles
+# Synop: display a list of articles suitable for admin purposes
 @articles_mod.route("/edit")
 def update_articles():
     arts = Article.query.all()
     return render_template("update_articles.html", articles = arts)
 
-
+# ##############################################################################
+# Name: add_article
+# Synop: add an article submitted by authenticated user
 @articles_mod.route("/add", methods=['POST', 'GET'])
 def add_article():
+    if request.method == 'POST':
+        art = Article()
+        art.author_id = current_user.id
+        art.body = request.form.get("body")
+        art.css = request.form.get("css")
+        art.javascript_before = request.form.get("javascript_before")
+        art.javascript_after = request.form.get("javascript_after")
+        art.title = request.form.get("title")
+        art.author_blurb = request.form.get("author_blurb")
+        
+        base_app.db.session.add(art)
+        base_app.db.session.commit()
+    
     return render_template("add_article.html")
 
+# ##############################################################################
+# Name: edit_article
+# Synop: update the specified article with the changes submitted via POST
 @articles_mod.route("/edit/<int:articleid>")
 def edit_article(articleid):
     return default()
 
+# ##############################################################################
+# Name: delete_article
+# Synpo: obvious
 @articles_mod.route("/delete/<int:articleid>")
 def delete_article(articleid):
     return default();
 
+# ##############################################################################
+# Name: add_article_comment
+# Synop: obvious
 @articles_mod.route("/addcomment/<int:articleid>", methods=['POST'])
 def add_article_comment(articleid):
-    
     return default()
